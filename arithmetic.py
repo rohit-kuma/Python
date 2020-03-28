5b077e4f7f048046c7e18372cc3c52a3a8639a1b
Rohit Kumar <rohit.k42@gmail.com>
2020-03-28 13:02:43 +0530

    Update arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -15,3 +15,17 @@
 # Less Than:        3 < 2
 # Greater or Equal: 3 >= 2
 # Less or Equal:    3 <= 2
+
+print(abs(-3))
+print(round(3.75))
+print(round(3.75,1))
+
+num_1 = 2
+num_2 = 3
+print(num_1 == num_2)
+
+num_1 = "100"
+num_2 = "200"
+num_1 = int(num_1)
+num_2 = int(num_2)
+print(num_1 + num_2)
