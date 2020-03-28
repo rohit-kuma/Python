a08ea0e0ec2d236430d867f853963c14fccbb1dd
Rohit Kumar <rohit.k42@gmail.com>
2020-03-28 19:25:02 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,18 @@
+nums = [1, 2, 3, 4, 5]
+for num in nums:
+    if 3 == num:
+        print("Found!")
+        break
+    print(num)
+
+for x in range(10):
+    print(x)
+
+
+for x in range(1,11):
+    print(x)
+
+x = 0
+while x < 10:
+    print(x)
+    x += 1
